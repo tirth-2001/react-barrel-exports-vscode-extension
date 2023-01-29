de5d4521242a00a221fdb18848524dcf581939b4
Tirth Patel <tirthgpatel.27@gmail.com>
2023-01-29 19:56:58 +0530

    feat: typescript support added

--- src/extension.ts
+++ src/extension.ts
@@ -15,10 +15,37 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand(
 		'barrel-export.generate',
 		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
+			// vscode.window.showInformationMessage('Hi from barrel export')
 
-			vscode.window.showInformationMessage('Hi from barrel export')
+			const editor = vscode.window.activeTextEditor
+			if (!editor) {
+				return // No open text editor
+			}
+
+			// Promt user for component name
+			vscode.window
+				.showInputBox({
+					prompt: 'Enter component name',
+					placeHolder: 'Component name',
+				})
+				.then((componentName) => {
+					if (!componentName) {
+						return
+					}
+					const isTypescript = checkIsTypescriptProject(editor)
+
+					createNewFolder(editor, componentName)
+					createNewComponentFile(editor, componentName, isTypescript)
+					createNewIndexFile(editor, componentName, isTypescript)
+
+					const textToInsert = `export * from './${componentName}'`
+
+					editor.edit((editBuilder) => {
+						editBuilder.insert(editor.selection.start, textToInsert)
+						// remove the selected text
+						editBuilder.delete(editor.selection)
+					})
+				})
 		}
 	)
 
@@ -27,3 +54,78 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+// create new folder in the same directory as the current file and name it as the component name
+const createNewFolder = (editor: vscode.TextEditor, componentName: string) => {
+	const currentFileUri = editor.document.uri
+	const currentFileDir = currentFileUri.fsPath.split('/').slice(0, -1).join('/')
+	const newDir = `${currentFileDir}/${componentName}`
+	vscode.workspace.fs.createDirectory(vscode.Uri.file(newDir))
+}
+
+// create new file in the new folder and name it as the component name
+const createNewComponentFile = (
+	editor: vscode.TextEditor,
+	componentName: string,
+	isTypescript: boolean
+) => {
+	const currentFileUri = editor.document.uri
+	const currentFileDir = currentFileUri.fsPath.split('/').slice(0, -1).join('/')
+	const newDir = `${currentFileDir}/${componentName}`
+	const newFile = `${newDir}/${componentName}.${isTypescript ? 'tsx' : 'jsx'}`
+
+	console.log('isTypescript', isTypescript, editor.document.fileName)
+
+	const textToInsert = generateComponentBoilerPlate(componentName, isTypescript)
+
+	vscode.workspace.fs.writeFile(
+		vscode.Uri.file(newFile),
+		new TextEncoder().encode(textToInsert)
+	)
+}
+
+// create new file in the new folder and name it as index.ts
+const createNewIndexFile = (
+	editor: vscode.TextEditor,
+	componentName: string,
+	isTypescript: boolean
+) => {
+	const currentFileUri = editor.document.uri
+	const currentFileDir = currentFileUri.fsPath.split('/').slice(0, -1).join('/')
+	const newDir = `${currentFileDir}/${componentName}`
+	const newFile = `${newDir}/index.${isTypescript ? 'ts' : 'js'}`
+
+	const textToInsert = `export * from './${componentName}'`
+
+	// insert the text in the new file
+	vscode.workspace.fs.writeFile(
+		vscode.Uri.file(newFile),
+		new TextEncoder().encode(textToInsert)
+	)
+}
+
+const generateComponentBoilerPlate = (
+	componentName: string,
+	isTypescript?: boolean
+) => {
+	const typescriptContent = `import React from 'react'
+
+export interface ${componentName}Props {}
+
+export const ${componentName}: React.FC<${componentName}Props> = () => {
+	return <div>${componentName} works!</div>
+}`
+	const javascriptContent = `import React from 'react'
+
+export const ${componentName} = () => {
+	return <div>${componentName} works!</div>
+}`
+	return isTypescript ? typescriptContent : javascriptContent
+}
+
+const checkIsTypescriptProject = (editor: vscode.TextEditor) => {
+	return editor.document.fileName.endsWith('.ts') ||
+		editor.document.fileName.endsWith('.tsx')
+		? true
+		: false
+}
